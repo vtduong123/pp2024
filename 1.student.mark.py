22dc800bdf9e86b842ec666ae8b33e67b044b849
vtduong123 <duong2003.dv@gmail.com>
2024-01-07 16:47:40 +0700

    Add function

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -9,3 +9,20 @@ def input_infos(args):
     item['name'] = input("Enter Name: ")
     item['DoB'] = input("Enter Date of Birth (DoB): ")
     return item
+
+# Function to input student marks based on the course id
+def input_mark(student, courses):
+    course_id = int(input("Enter Course ID to enter mark for the student: "))
+    mark = float(input("Enter Mark for the student: "))
+    student["marks"][course_id] = mark
+
+# Function to list students
+def list_students(students):
+    if not students:
+        print("There aren't any students yet")
+    else:
+        print("Here is the student list: ")
+        for i, student in enumerate(students):
+            print(f"{i+1}. {student['id']} - {student['name']} - {student['DoB']}")
+            if "marks" in student:
+                print("Marks (Course Id - Mark): ", student["marks"])
